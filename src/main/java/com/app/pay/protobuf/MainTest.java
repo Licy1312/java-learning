a40d8b2a770a206bd4b3c9bdadba6195e82fdd95
lichangya <691065448@qq.com>
2020-09-02 16:45:23 +0800

    note

--- src/main/java/com/app/pay/protobuf/MainTest.java
+++ src/main/java/com/app/pay/protobuf/MainTest.java
@@ -13,7 +13,7 @@ public class MainTest {
         RequestOrder.PayOrderInfo payOrderInfo = RequestOrder.PayOrderInfo.newBuilder()
                 .setRequestId(2019081311)
                 .setCheckBit("abc123")
-                .setBizContent("{\"TxnSrlNo\":\"20190808100023560557\",\"PlatCstId\":\"270464480\",\"TxnDt\":\"20190808\",\"TxnTs\":\"100023560\",\"AcctNo\":\"6236430901115900352\"}")
+                .setBizContent("{\"TxnSrlNo\":\"1234567890\",\"PlatCstId\":\"270464480\",\"TxnDt\":\"20190808\",\"TxnTs\":\"100023560\",\"AcctNo\":\"1234567890\"}")
                 .build();
 
         byte[] payOrderByte = payOrderInfo.toByteArray();
